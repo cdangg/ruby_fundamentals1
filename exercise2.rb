c68ebb6ffbf0bc3d0e9053d108db2cdaf7c7a671
Celia Dang <dang.celia@gmail.com>
2014-01-14 13:12:37 -0500

    Added exercise2

---
+++ exercise2.rb
@@ -0,0 +1,16 @@
+#1. A good tip for a 55 dollar meal is 20%
+puts 55 * 1.20
+
+#2. String and Integer
+puts "Hello"
+puts 1.to_i
+
+#3. 45628 * 7839
+a = 45628
+b = 7839
+
+puts "The value of a multiplied by b is #{a*b}"
+
+#4. Value of the expression
+puts (true && false) || (false && true) || !(false && false)
+ 
